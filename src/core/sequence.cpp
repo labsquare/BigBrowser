afaa5570a90baa4673f8b4a93ebfd6b4cc637858
Bioinfo <bioinfo@localhost.localdomain>
2016-03-15 11:13:02 +0100

    create new Sequence class

--- src/core/sequence.cpp
+++ src/core/sequence.cpp
@@ -4,71 +4,38 @@
 
 namespace big {
 
-    namespace core {
+namespace core {
 
-	namespace sequence
-	{
-	    Transform* Transform::m_instance = nullptr;
+Sequence::Sequence(const char *data)
+    :QByteArray(data)
+{
 
-	    Transform::Transform()
-	    {
-		this->mComplement['-'] = '-'; // gap
-		this->mComplement['.'] = '.'; // gap
-		this->mComplement['A'] = 'T'; // Adenine
-		this->mComplement['B'] = 'B'; // C or G or T
-		this->mComplement['C'] = 'G'; // Cytosine
-		this->mComplement['D'] = 'D'; // A or G or T
-		this->mComplement['G'] = 'C'; // Guanine
-		this->mComplement['H'] = 'H'; // A or C or T
-		this->mComplement['K'] = 'K'; // G or T
-		this->mComplement['M'] = 'M'; // A or C
-		this->mComplement['N'] = 'N'; // any base
-		this->mComplement['R'] = 'R'; // A or G
-		this->mComplement['S'] = 'S'; // G or C
-		this->mComplement['T'] = 'A'; // Thymine
-		this->mComplement['U'] = 'A'; // Uracil
-		this->mComplement['V'] = 'V'; // A or C or G
-		this->mComplement['W'] = 'W'; // A or T
-		this->mComplement['Y'] = 'Y'; // C or G
-		this->mComplement['a'] = 't'; // Adenine
-		this->mComplement['b'] = 'b'; // C or G or T
-		this->mComplement['c'] = 'g'; // Cytosine
-		this->mComplement['d'] = 'd'; // A or G or T
-		this->mComplement['g'] = 'c'; // Guanine
-		this->mComplement['h'] = 'h'; // A or C or T
-		this->mComplement['k'] = 'k'; // G or T
-		this->mComplement['m'] = 'm'; // A or C
-		this->mComplement['n'] = 'n'; // any base
-		this->mComplement['r'] = 'r'; // A or G
-		this->mComplement['s'] = 's'; // G or C
-		this->mComplement['t'] = 'a'; // Thymine
-		this->mComplement['u'] = 'a'; // Uracil
-		this->mComplement['v'] = 'v'; // A or C or G
-		this->mComplement['w'] = 'w'; // A or T
-		this->mComplement['y'] = 'y'; // C or G
-	    }
-	
-	    Transform* Transform::instance()
-	    {
-		if(Transform::m_instance == nullptr)
-		{
-		    Transform::m_instance = new Transform();
-		}
+}
 
-		return Transform::m_instance;
-	    }
+void Sequence::reverse()
+{
 
-	    char Transform::complement(char nuc)
-	    {
-		return this->mComplement[nuc];
-	    }
+}
 
-	    QChar Transform::complement(QChar& nuc)
-	    {
-		return this->complement(nuc.toLatin1());
-	    }
-	} // end of namespace sequence
-    } // end of namespace core
+Sequence Sequence::reversed() const
+{
+
+    return Sequence();
+}
+
+QString Sequence::name() const
+{
+    return mName;
+}
+
+void Sequence::setName(const QString &name)
+{
+    mName = name;
+}
+
+
+
+} // end of namespace core
 } // end of namespace big
 
 
--- src/core/sequence.h
+++ src/core/sequence.h
@@ -1,192 +1,37 @@
 #ifndef SEQUENCE_H
 #define SEQUENCE_H
+#include <QtCore>
 
-/* std include */
-#include <iostream>
-
-/* Qt include*/
-#include <QByteArray>
-#include <QChar>
 
 namespace big {
-    namespace core {
-	namespace sequence {
-	    /*!
-	     * \brief Transform
-	     * Used for perform some biological operation on sequence
-	     */
-	    class Transform
-	    {
-	    private:
+namespace core {
 
-		Transform();
+class Sequence : public QByteArray
+{
+public:
+    enum Type {
+        ADN     = 0x01,
+        ARN     = 0x02,
+        PROTEIN = 0x03
+    };
 
-	    public:
+    Sequence(const char *data);
 
-		/*!
-		 * \brief instance
-		 *
-		 * Get a pointer on Transform object
-		 *
-		 * \return Transform pointer
-		 */
-		static Transform* instance();
-
-		/*!
-		 * \brief complement
-		 * \return the complement sequence
-		 */
-		template<typename SOURCE>
-		    SOURCE complement(SOURCE src)
-		{
-		    SOURCE c = SOURCE(src.length(), 'A');
-
-		    auto src_begin = src.begin();
-		    auto src_end = src.end();
-		    auto c_begin = c.begin();
-		    for(; src_begin <= src_end; src_begin++, c_begin++)
-		    {
-			*c_begin = this->complement(*src_begin);
-		    }
-
-		    return c;
-		}
-
-		/*!
-		 * \brief complement
-		 *
-		 * Return the complement of nucleotide.
-		 *
-		 * For Adenine return always Thymine no Uracil.
-		 * If IUPAC alphabet is use in sequence we return the same letter.
-		 * Case is keep
-		 *
-		 * If letter is not in IUPAC Alphabet behavior of function is
-		 * undefine
-		 *
-		 * \param nucleotide
-		 * \return the complement of original nucleotide
-		 */
-		char complement(char nuc);
-		QChar complement(QChar& nuc);
-
-		/*!
-		 * \brief reverse
-		 * \return the reverse sequence
-		 */
-		template<typename SOURCE>
-		    SOURCE reverse(SOURCE src)
-		{
-		    SOURCE c = SOURCE(src.length(), 'A');
-
-		    auto src_begin = src.begin();
-		    auto src_end = src.end(); src_end--;
-		    auto c_begin = c.begin();
-		    auto c_end = c.end(); c_end--;
-		    for(; src_begin <= src_end; src_begin++, src_end--, c_begin++, c_end--)
-		    {
-			*c_begin = *src_end;
-			*c_end = *src_begin;
-		    }
-
-		    return c;
-		}
-
-		/*!
-		 * \brief reverseComplement
-		 * \return the reverse complement sequence
-		 */
-		template<typename SOURCE>
-		    SOURCE reverseComplement(SOURCE src)
-		{
-		    SOURCE c = SOURCE(src.length(), 'A');
-
-		    auto src_begin = src.begin();
-		    auto src_end = src.end(); src_end--;
-		    auto c_begin = c.begin();
-		    auto c_end = c.end(); c_end--;
-		    for(; src_begin <= src_end; src_begin++, src_end--, c_begin++, c_end--)
-		    {
-			*c_begin = this->complement(*src_end);
-			*c_end = this->complement(*src_begin);
-		    }
-
-		    return c;
-		}
-
-	    private:
-
-		static Transform* m_instance;
-		unsigned char mComplement[122];
-	    };
+    void reverse();
+    Sequence reversed() const;
 
 
-	    template<typename SEQ>
-		SEQ transcribe(SEQ &orig)
-	    {
-		SEQ next();
-		next.sequence = orig.sequence;
-	        
-		return next;
-	    }
+    QString name() const;
+    void setName(const QString &name);
 
-	    template<typename SEQ>
-	        SEQ backTranscribe(SEQ &orig)
-	    {
-	        SEQ next;
+private:
+    // do not use pointer. Not virtual destructor
+QString mName;
 
-		next = orig;
 
-		return next;
-	    }
+};
 
-	    template<typename SEQ>
-		SEQ translate(SEQ &orig)
-	    {
-	        SEQ next;
-
-	        next = orig;
-
-		return next;
-	    }
-	    
-	    template<typename SEQ>
-	        SEQ complement(SEQ &orig)
-	    {
-	        SEQ next;
-
-	        next = Transform::instance()->complement(orig);
-
-		return next;
-	    }
-
-	    template<typename SEQ>
-	        SEQ reverse(SEQ  &orig)
-	    {
-	        SEQ next;
-
-	        next = Transform::instance()->reverse(orig);
-
-		return next;
-	    }
-
-	    template<typename SEQ>
-		SEQ reverseComplement(SEQ &orig)
-	    {
-	        SEQ next;
-
-	        next = Transform::instance()->reverseComplement(orig);
-
-		return next;
-	    }
-	} // end of namespace sequence
-
-	/*!
-	 * Definition of Sequence with specialisation of sequence::Base
-	 */
-	typedef QByteArray Sequence;
-
-    } // end of namespace core
+} // end of namespace core
 } // end of namespace big
 
 #endif // SEQUENCE_H
